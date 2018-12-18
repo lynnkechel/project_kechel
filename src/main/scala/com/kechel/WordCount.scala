88f72b429e72c95a38d8b3645ab5523567674c77
lynnkechel <18246182009@163.com>
2018-12-18 17:32:31 +0800

    first commit

---
+++ src/main/scala/com/kechel/WordCount.scala
@@ -0,0 +1,21 @@
+package com.kechel
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    val inpath = "D:\\test\\spark\\000.txt"
+    val outpath = "D:\\test\\spark\\out"
+
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    sc.textFile(inpath).flatMap(_.split("\t")).map((_ , 1))
+      .reduceByKey(_+_, 1).sortBy(_._2, false).saveAsTextFile(outpath)
+
+    sc.stop()
+  }
+
+}
