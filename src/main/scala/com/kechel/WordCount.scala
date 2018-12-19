6a4d485efbf9fc959ecdb9ef6565e2ca15c74ee0
lynnkechel <18246182009@163.com>
2018-12-19 09:47:10 +0800

    idea commit

--- src/main/scala/com/kechel/WordCount.scala
+++ src/main/scala/com/kechel/WordCount.scala
@@ -8,12 +8,15 @@ object WordCount {
 
     //spark实现wordcount
 
+    //设置输入输出路径
     val inpath = "D:\\test\\spark\\000.txt"
     val outpath = "D:\\test\\spark\\out"
 
+    //构建sparkConf
     val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
+    //wordcount
     sc.textFile(inpath).flatMap(_.split("\t")).map((_ , 1))
       .reduceByKey(_+_, 1).sortBy(_._2, false).saveAsTextFile(outpath)
 
